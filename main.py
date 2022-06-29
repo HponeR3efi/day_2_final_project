c3415d9675a00011fbe79bdc2766ea8accfcb61b
Hpone Myat <93415572+HponeR3efi@users.noreply.github.com>
2022-06-29 23:39:06 +0630

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,21 @@
 
 print ("Welcome to the tip calculator")
 
-bill = input("What was the total bill? $")
-percentage = input("What percentage tip woruld you like to hive? 10, 12, or 15?  ")
-people_who_will_split_bill = input("How many people to split the bill? ")
+bill = float(input("What was the total bill? $"))
+percentage = int(input("What percentage tip woruld you like to hive? 10, 12, or 15?  "))
+people_who_will_split_bill = int(input("How many people to split the bill? "))
 
-percentage_upon_bill = int(percentage) / 100
+percentage_calculation = percentage / 100
 
-add_percentage_to_bill = float(bill) * percentage_upon_bill
+percentage_on_bill = bill * percentage_calculation 
 
-total_bill = float(bill) + float(add_percentage_to_bill)
+total_bill_with_percentage = bill + percentage_on_bill
 
-spliting_the_total_bill_to_respective_number_of_people = round(float(total_bill) / int(people_who_will_split_bill),2)
+total_bill_splited_by_people = total_bill_with_percentage / people_who_will_split_bill
 
-print(f"Each person should pay: ${spliting_the_total_bill_to_respective_number_of_people}")
+# there are two ways to get decimal numbers in python , here are these two examples
+
+final_bill = round(total_bill_splited_by_people,2)
+final_bill = "{:.2f}".format(total_bill_splited_by_people)
+
+print(f"Each person should pay: ${final_bill}")
